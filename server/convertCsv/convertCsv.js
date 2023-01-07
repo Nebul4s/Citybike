f7ad8a93143124a8c4620d0eee5e2c2307a1d257
Nebul4s <j.hiltula24@gmail.com>
2023-01-07 07:13:54 +0200

    Created a file converter that converts and imports data from csv file to mongoDB

---
+++ server/convertCsv/convertCsv.js
@@ -0,0 +1,100 @@
+const fs = require("fs");
+const csv = require("csvtojson");
+const { Transform } = require("stream");
+const { pipeline } = require("stream/promises");
+const mongoose = require("mongoose");
+const Location = require("../model/LocationsSchema");
+const Journey = require("../model/JourneySchema");
+const dotenv = require("dotenv");
+const bufferingObjectStream = require("buffering-object-stream");
+
+const convertAndImportToDatabase = async () => {
+  dotenv.config({ path: "../config.env" });
+  const collection = "journeys";
+  const DB = process.env.DATABASE.replace("<password>", process.env.PASSWORD);
+
+  mongoose.set("strictQuery", false);
+  mongoose.connect(DB, () => {
+    console.log("DB connection success");
+  });
+
+  const readStream = fs.createReadStream("./2021-07.csv");
+
+  const filter = new Transform({
+    objectMode: true,
+    transform(item, enc, callback) {
+      if (
+        item.DurationSec >= 10 &&
+        item.CoveredDistanceMeters >= 10 &&
+        collection === "journeys"
+      ) {
+        callback(null, item);
+        return;
+      }
+      if (collection === "locations") {
+        callback(null, item);
+        return;
+      }
+      callback(null);
+    },
+  });
+
+  const saveItemsToDb = new Transform({
+    objectMode: true,
+    async transform(items, enc, cb) {
+      if (collection === "locations") {
+        await Location.bulkWrite(
+          items.map((item) => ({
+            insertOne: {
+              document: item,
+            },
+          }))
+        );
+        cb();
+      }
+      if (collection === "journeys") {
+        await Journey.bulkWrite(
+          items.map((item) => ({
+            insertOne: {
+              document: item,
+            },
+          }))
+        );
+        cb();
+      }
+    },
+  });
+
+  try {
+    await pipeline(
+      readStream,
+      collection === "journeys"
+        ? csv(
+            {
+              noheader: false,
+              headers: [
+                "Departure",
+                "Return",
+                "DepartureStationId",
+                "DepartureStationName",
+                "ReturnStationId",
+                "ReturnStationName",
+                "CoveredDistanceMeters",
+                "DurationSec",
+              ],
+            },
+            { objectMode: true }
+          )
+        : csv({}, { objectMode: true }),
+      filter,
+      bufferingObjectStream(500),
+      saveItemsToDb
+    );
+    console.log("Stream finished");
+    process.exit();
+  } catch (error) {
+    console.log("Stream ended with error ", error);
+  }
+};
+
+convertAndImportToDatabase();
