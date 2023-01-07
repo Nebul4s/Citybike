ae9d6139f871f271802d90673c5d1b3836511682
Nebul4s <j.hiltula24@gmail.com>
2023-01-07 07:12:11 +0200

    Created mongoose Schemas

---
+++ server/model/JourneySchema.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const journeySchema = new mongoose.Schema({
+  Departure: {
+    type: Date,
+    required: true,
+  },
+  Return: {
+    type: Date,
+    required: true,
+  },
+  DepartureStationId: {
+    type: Number,
+    required: true,
+  },
+  DepartureStationName: {
+    type: String,
+    required: true,
+  },
+  ReturnStationId: {
+    type: Number,
+    required: true,
+  },
+  ReturnStationName: {
+    type: String,
+    required: true,
+  },
+  CoveredDistanceMeters: {
+    type: Number,
+    required: true,
+  },
+  DurationSec: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Journey = mongoose.model("Journey", journeySchema);
+
+module.exports = Journey;
---
+++ server/model/LocationsSchema.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const locationsSchema = new mongoose.Schema({
+  FID: {
+    type: Number,
+  },
+  ID: {
+    type: Number,
+  },
+  Nimi: {
+    type: String,
+  },
+  Namn: {
+    type: String,
+  },
+  Name: {
+    type: String,
+  },
+  Osoite: {
+    type: String,
+  },
+  Adress: {
+    type: String,
+  },
+  Kaupunki: {
+    type: String,
+  },
+  Stad: {
+    type: String,
+  },
+  Operaattor: {
+    type: String,
+  },
+  Kapasiteet: {
+    type: Number,
+  },
+  x: {
+    type: String,
+  },
+  y: {
+    type: String,
+  },
+});
+
+const Location = mongoose.model("Location", locationsSchema);
+
+module.exports = Location;
